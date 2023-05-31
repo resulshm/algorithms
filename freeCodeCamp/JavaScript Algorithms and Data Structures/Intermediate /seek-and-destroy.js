5a34aa08514407a7c0528cab7097601d390d8edd
resulshm <resulshamuhammedov@gmail.com>
2023-05-31 12:37:02 +0500

    added solutions of first problems

---
+++ freeCodeCamp/JavaScript Algorithms and Data Structures/Intermediate /seek-and-destroy.js
@@ -0,0 +1,13 @@
+function destroyer(arr) {
+    let result = []
+    let input = arguments[0]
+    let elems = [...arguments].slice(1)
+    for (let i = 0; i<input.length; i++) {
+      if (elems.indexOf(input[i]) == -1) {
+        result.push(input[i])
+      }
+    }
+    return result;
+  }
+  
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
