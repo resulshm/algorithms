31c22b8149d7574fdfee8cb6d77934cd19e4bbf3
resulshm <resulshamuhammedov@gmail.com>
2023-06-02 21:10:43 +0500

    wherefore-art-thou problem from freeCodeCamp

---
+++ freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Intermediate /wherefore-art-thou.js
@@ -0,0 +1,11 @@
+function whatIsInAName(collection, source) {
+    const keys = Object.keys(source)
+    for (let i =0; i<keys.length; i++) {
+        collection = collection.filter(obj => (obj.hasOwnProperty(keys[i]) && obj[keys[i]]==source[keys[i]]))
+    }
+    return collection
+}
+
+const result = whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 });
+
+console.log(result)
