e616ec05bab39bcd731d06e174569871afd99ca1
resulshm <resulshamuhammedov@gmail.com>
2023-06-12 18:31:35 +0500

    solved drop it challange

---
+++ freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Intermediate /drop-it.js
@@ -0,0 +1,12 @@
+function dropElements(arr, func) {
+    for (let i =0; i< arr.length; i++) {
+        if (func(arr[i])) {
+            return arr.slice(i)
+        }
+    }
+    return [];
+  }
+  
+let result = dropElements([1, 2, 3,4], function(n) {return n >=3; });
+
+console.log(result)
