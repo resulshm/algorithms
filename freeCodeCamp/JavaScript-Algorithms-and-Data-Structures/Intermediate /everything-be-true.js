10bf6c196497c2f1ec412f553716c1754e2f94e7
resulshm <resulshamuhammedov@gmail.com>
2023-06-13 16:48:10 +0500

    solved everything be true challange

---
+++ freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Intermediate /everything-be-true.js
@@ -0,0 +1,12 @@
+function truthCheck(collection, pre) {
+    for (let obj of collection) {
+        if (!Boolean(obj[pre])){
+            return false
+        }
+    }
+    return true;
+  }
+  
+let result = truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
+
+console.log(result)
