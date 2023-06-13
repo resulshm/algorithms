d81da1476ed9d4cf57388fcc75cfc187a6615ba2
resulshm <resulshamuhammedov@gmail.com>
2023-06-13 16:39:13 +0500

    solved binary agents challange

---
+++ freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Intermediate /binary-agents.js
@@ -0,0 +1,13 @@
+function binaryAgent(str) {
+    let binaryLetters = str.split(" ")
+    let lettersCode = []
+    binaryLetters.forEach((value, index) => {
+        let decimalLetter = parseInt(value, 2)
+        lettersCode.push(decimalLetter)
+    })
+    return String.fromCharCode(...lettersCode);
+  }
+  
+let result = binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+console.log(result)
