6ade9410ba9d2a7b7de2e4601df3cc23ee54de0b
resulshm <resulshamuhammedov@gmail.com>
2023-06-14 15:41:53 +0500

    solved make a person challange

---
+++ freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Intermediate /make-a-person.js
@@ -0,0 +1,25 @@
+const Person = function(firstAndLast) {
+    let fullname = firstAndLast
+    this.getFullName = () => {
+      return fullname;
+    };
+    this.getFirstName = () => {
+        return fullname.split(" ")[0]
+    }
+    this.getLastName = () => {
+        return fullname.split(" ")[1]
+    }
+    this.setFirstName = (newName) => {
+        fullname = newName + " " + fullname.split(" ")[1]
+    }
+    this.setLastName = (newName) => {
+        fullname = fullname.split(" ")[0] + " " + newName
+    }
+    this.setFullName = (newName) => {
+        fullname = newName
+    }
+};
+  
+const bob = new Person('Bob Ross');
+let result = bob.getFullName();
+console.log(result)
