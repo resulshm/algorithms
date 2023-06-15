ed07a6fa82d7f11ed32f2b8da818a246c4296829
resulshm <resulshamuhammedov@gmail.com>
2023-06-15 16:43:05 +0500

    solved Caesar Cipher challange

---
+++ freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/finalProjects/caesar-cipher.js
@@ -0,0 +1,18 @@
+function rot13(str) {
+    let ansCodes = [];
+    for (let i = 0; i<str.length; i++) {
+        let charCode = str.charCodeAt(i)
+        if (65 <= charCode && charCode <= 90){
+            if (charCode + 13 > 90) {
+                charCode = 64 + charCode - 90
+            }
+            charCode = charCode + 13
+        }
+        ansCodes.push(charCode)
+    }
+    return String.fromCharCode(...ansCodes);
+}
+  
+let result = rot13("SERR PBQR PNZC");
+console.log(result)
+
