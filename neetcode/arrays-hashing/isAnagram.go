80036f1ff6db3d66bb2de6a5174a7746f8b8ec3b
Resul Shamuhammedov <resulshamuhammedov@gmail.com>
2024-05-02 11:55:13 +0500

    solved Leetcode 242. Valid Anagram

---
+++ neetcode/arrays-hashing/isAnagram.go
@@ -0,0 +1,44 @@
+package arrayshashing
+
+/*
+	242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
+
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+*/
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	letterCount := make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		letterCount[s[i]]++
+	}
+
+	for i := 0; i < len(t); i++ {
+		if letterCount[t[i]] == 0 {
+			return false
+		}
+		letterCount[t[i]]--
+	}
+
+	return true
+}
