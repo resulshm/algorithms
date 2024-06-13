f3b278074364ea20a975a1c21fe6f49689cf9178
Resul Shamuhammedov <resulshamuhammedov@gmail.com>
2024-06-13 14:28:02 +0500

    solved: 560. Subarray Sum Equals K

---
+++ leetcode/medium/subarraySum.go
@@ -0,0 +1,44 @@
+package medium
+
+/*
+	560. Subarray Sum Equals K
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+
+Example 1:
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+Example 2:
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+
+Constraints:
+- 1 <= nums.length <= 2 * 10
+- -1000 <= nums[i] <= 1000
+- -107 <= k <= 107
+*/
+
+func subarraySum(nums []int, k int) int {
+	sumsCount := make(map[int]int)
+	currentSum := 0
+	ans := 0
+	for _, num := range nums {
+		currentSum += num
+
+		if diffCount, isExist := sumsCount[currentSum-k]; isExist {
+			ans += diffCount
+		}
+
+		if currentSum == k {
+			ans += 1
+		}
+
+		sumsCount[currentSum]++
+	}
+	return ans
+}
